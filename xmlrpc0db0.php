7fece99fa98c4c2a0eabbbe97852dbbfa77e04b7
Zarin Code labs <host01@zarin.solutions>
2023-12-11 16:46:50 +0500

    update

--- xmlrpc0db0.php
+++ xmlrpc0db0.php
@@ -2,13 +2,13 @@
 	<service>
 		<engineName>WordPress</engineName>
 		<engineLink>https://wordpress.org/</engineLink>
-		<homePageLink>https://designer1.zarin.solutions</homePageLink>
+		<homePageLink>https://hassanconsultants.com</homePageLink>
 		<apis>
-			<api name="WordPress" blogID="1" preferred="true" apiLink="https://designer1.zarin.solutions/xmlrpc.php" />
-			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://designer1.zarin.solutions/xmlrpc.php" />
-			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://designer1.zarin.solutions/xmlrpc.php" />
-			<api name="Blogger" blogID="1" preferred="false" apiLink="https://designer1.zarin.solutions/xmlrpc.php" />
-				<api name="WP-API" blogID="1" preferred="false" apiLink="https://designer1.zarin.solutions/wp-json/" />
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://hassanconsultants.com/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://hassanconsultants.com/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://hassanconsultants.com/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://hassanconsultants.com/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://hassanconsultants.com/wp-json/" />
 			</apis>
 	</service>
 </rsd>
